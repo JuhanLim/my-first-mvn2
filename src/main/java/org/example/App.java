f9c9b81dd54757531ee26c3bcda0fb9f1b3c5658
Juhan Lim <dlawhks@naver.com>
2023-08-24 16:45:41 +0900

    계산기 코드 추가 , 테스트 코드 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+
+        // 계산기 사용
+        // Calculator calc = new Calculator();
+     //  int result = calc.add(5,10);
+     //   System.out.println("Result="+ result);
+
     }
+
+
 }
